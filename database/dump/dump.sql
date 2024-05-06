5cbbb4183aaace95e2277bf84e422bf87e00ae2a
NotTaeheeKim <not.taehee.kim@gmail.com>
2024-05-06 12:06:16 +0400

    testing database definition
    copied sql definition and manipulation into dump.sql
    
    should handle the FOREIGN KEY error in dump.sql

--- database/dump/dump.sql
+++ database/dump/dump.sql
@@ -1,3 +1,28 @@
-CREATE TABLE PATHS (
-  id int(11)
-)
+CREATE TABLE team (
+  TeamID INT PRIMARY KEY,
+  TeamName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE token (
+  TokenID INT PRIMARY KEY,
+  TokenValue VARCHAR(255) NOT NULL,
+  TokenType VARCHAR(255)
+);
+
+CREATE TABLE user (
+  UserID INT PRIMARY KEY,
+  UserName VARCHAR(255) NOT NULL,
+  PW VARCHAR(255) NOT NULL
+  TeamID INT,
+  FOREIGN KEY (TeamID) REFERENCES team(TeamID)
+);
+
+CREATE TABLE userToToken (
+  UserID INT,
+  TokenID INT,
+  FOREIGN KEY (UserID) REFERENCES user(UserID),
+  FOREIGN KEY (TokenID) REFERENCES token(TokenID),
+  PRIMARY KEY (UserID, TokenID)
+);
+INSERT INTO team VALUES(1,"Investment Team 1");
+INSERT INTO user VALUES(1,"TaeheeKim","Taehee Kim","SuperSecretPassword",1);
